feba6e8f7dd61057fdaf0a922003de79566465f8
jenkin90@gmail.com <jenkin90@gmail.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2011-12-06 17:58:54 +0000

    intento de correccion de errores en los directorios

---
+++ preguntasus/index.php
@@ -0,0 +1,47 @@
+<?php
+require "lib/autoloader.php";
+echo show::mostrar("cabeceraHTML");
+echo show::mostrar("cabecera");
+
+//$el=new ElementoImpl();
+//foreach($el as $e){
+//    print_r($e->current());
+//}
+
+?>
+<div id="contenedor_cuerpo">
+    ContenedorCuerpo
+    <div id="preguntas">
+        ContenedorPreguntas
+        <div class="pregunta">
+            Pregunta
+            <div class="usuario_que_pregunta">
+                Usuario preguntador
+            </div>
+            <div class="preguntaConcreta">
+                Contenedor pregunta concreta
+                <div class="tituloPregunta">
+                    Titulo de la pregunta
+                </div>
+                <div class="estadisticasPregunta">
+                    Estadisticas de la pregunta
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="columna">
+        Columna
+        <div id="cuadro_busqueda">
+            Cuadro de busqueda
+        </div>
+        <div id="etiquetas_mas_frecuentes">
+            Nube de etiquetas
+        </div>
+    </div>
+</div>
+    <?php
+    echo show::mostrar("pie");
+    ?>
+<?php
+echo show::mostrar("pieHTML");
+?>
