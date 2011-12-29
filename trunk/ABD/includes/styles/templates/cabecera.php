b039caa45aa5b85ad6e3d71e04192bdb00932088
jenkin90@gmail.com <jenkin90@gmail.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2011-12-29 16:55:50 +0000

    cambiado la cabecera para que

--- trunk/ABD/includes/styles/templates/cabecera.php
+++ trunk/ABD/includes/styles/templates/cabecera.php
@@ -1,12 +1,14 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-	<head>
-		<title>Preguntas y respuestas US</title>
-		<!-- Importacion de css -->
-		<link type="text/css" rel="stylesheet" href="./includes/styles/css/preguntasus.css" />
-		<!-- Importacion de javascript -->
-		<?php if ( stripos($_SERVER['REQUEST_URI'], 'registrarse') !== FALSE) { ?>
-			<script type="text/javascript" src="./includes/styles/js/registrarse.js"></script>
-		<?php } ?>
-	</head>
-	<body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+
+    <head>
+        <meta http-equiv="content-type" content="text/html; utf-8" />
+        <title>Preguntas y respuestas US</title>
+        <!-- Importacion de css -->
+        <link type="text/css" rel="stylesheet" href="./includes/styles/css/preguntasus.css" />
+        <!-- Importacion de javascript -->
+        <?php if (stripos($_SERVER['REQUEST_URI'], 'registrarse') !== FALSE) { ?>
+            <script type="text/javascript" src="./includes/styles/js/registrarse.js"></script>
+        <?php } ?>
+    </head>
+    <body>
