172b9a322bade6e799dd159869bfa9d1678c12fb
albertosola@ono.com <albertosola@ono.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2011-12-31 14:57:30 +0000

    Control de errores y procesamiento de las nuevas respuestas para su insercion en la base de datos

---
+++ trunk/ABD/procesado/procesaNuevaRespuesta.php
@@ -0,0 +1,41 @@
+<?php
+require_once ("../entidades/Elemento.php");
+require_once ("../accesosBD/conexionesBD.php");
+require_once ("../accesosBD/accesosElemento.php");
+ 
+session_start();
+$nuevaRespuesta = $_SESSION['nuevaRespuesta'];
+
+if (isset($nuevaRespuesta)) {// si está creada en la sesion
+  $nuevaRespuesta['respuesta'] = trim($_REQUEST['mi-respuesta']);  
+  $_SESSION['nuevaRespuesta'] = $nuevaRespuesta;
+} else
+		Header("Location:../preguntaRespuesta.php");
+
+$errores = valida($nuevaRespuesta);
+
+if (!empty($errores)) {
+   		$_SESSION['errores'] = $errores;
+   		Header("Location:../preguntaRespuesta.php");
+}else{
+	$_SESSION['errores'] = "";
+	preparaInsercion($nuevaRespuesta);	
+	Header("Location:../index.php");
+}
+
+function valida($nuevaRespuesta) {
+	$error="";
+  	if (!(isset($nuevaRespuesta['respuesta']) && strlen($nuevaRespuesta['respuesta'])>0)) //Si la variable nuevaRespuesta esta vacia
+  	 $error = 'El campo <b>Respuesta</b> no puede ser vacio';  
+   	return $error;      
+}
+function preparaInsercion($nuevaRespuesta){
+	$conexion=crearConexion();
+	$elemento=new Elemento();
+	$elemento=$nuevaRespuesta['elemento'];
+	$elemento->cuerpo=$nuevaRespuesta['respuesta'];
+	$elemento->idrespuesta=$nuevaRespuesta['idelemento'];
+	insertarElemento($elemento,$conexion);
+	cerrarConexion($conexion);
+}
+?>
