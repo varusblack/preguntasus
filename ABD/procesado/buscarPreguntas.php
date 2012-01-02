96a82f1a19f5f7a3c5c9935b7fa582ca5df996e3
varusblack@gmail.com <varusblack@gmail.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-02 13:38:35 +0000

    Avanzando buscar

--- ABD/procesado/buscarPreguntas.php
+++ ABD/procesado/buscarPreguntas.php
@@ -1,32 +1,54 @@
 <?php
+	$tipoBusqueda=NULL;
+   	$cadena = $_REQUEST["palabras"];
+	$tag = $_REQUEST["tag"];
    
-	session_start();
-	$buscarPreguntas=$_SESSION["buscarPreguntas"];
-	$erroresBuscarPreguntas=$_SESSION["erroresBuscarPregunta"];
+   	$errores = array();
 	
-	if(isset($buscarPreguntas)){
-		$buscarPreguntas["palabras"] = $_REQUEST["palabras"];
-		$buscarPreguntas["tag"]=$_REQUEST["tag"];
-		
-		$_SESSION["buscarPalabras"]=$buscarPreguntas;
-		if(!isset($buscarPreguntas["palabras"]) && !isset($buscarPreguntas["tag"])){
-			$erroresBuscarPreguntas[1]="Los dos campos no pueden ser vacios";
-			Header("Location:buscar.php");
-			die();
-		}else{
-			if(isset($buscarPreguntas["palabras"]) && isset($buscarPreguntas["tag"])){
-				$_SESSION["tipobusqueda"]=1;
-			}
-			if(isset($buscarPreguntas["palabras"]) && !isset($buscarPreguntas["tag"])){
-				$_SESSION["tipobusqueda"]=2;
-			}
-			if(!isset($buscarPreguntas["palabras"]) && isset($buscarPreguntas["tag"])){
-				$_SESSION["tipobusqueda"]=3;
-			}			
-			Header("Location:buscar.php");
-			die();
-		}				
-	}    else{
-		echo "COPON DE NEGRO PA TI";
+	if(!isset($cadena) && !isset($tag)){
+		$errores[1]="Los dos campos no pueden ser vacios";
+	}	
+	if(isset($cadena) && isset($tag)){
+		$tipobusqueda=1;
 	}
+	if(isset($cadena) && !isset($tag)){
+		$tipobusqueda=2;
+	}
+	if(!isset($cadena) && isset($tag)){
+		$tipobusqueda=3;
+	}
+	
+// 	
+//    
+	// $buscarPreguntas=$_SESSION["buscarPreguntas"];
+	// $erroresBuscarPreguntas=$_SESSION["erroresBuscarPregunta"];
+// 	
+// 	
+// 	
+// 	
+// 	
+	// if(isset($buscarPreguntas)){
+		// $buscarPreguntas["palabras"] = $_REQUEST["palabras"];
+		// $buscarPreguntas["tag"]=$_REQUEST["tag"];
+// 		
+		// $_SESSION["buscarPalabras"]=$buscarPreguntas;
+		// if(!isset($buscarPreguntas["palabras"]) && !isset($buscarPreguntas["tag"])){
+			// $erroresBuscarPreguntas[1]="Los dos campos no pueden ser vacios";
+// 			
+		// }else{
+			// if(isset($buscarPreguntas["palabras"]) && isset($buscarPreguntas["tag"])){
+				// $_SESSION["tipobusqueda"]=1;
+			// }
+			// if(isset($buscarPreguntas["palabras"]) && !isset($buscarPreguntas["tag"])){
+				// $_SESSION["tipobusqueda"]=2;
+			// }
+			// if(!isset($buscarPreguntas["palabras"]) && isset($buscarPreguntas["tag"])){
+				// $_SESSION["tipobusqueda"]=3;
+			// }				
+		// }	
+		// Header("Location:buscar.php");
+		// die();			
+	// }    else{
+		// echo "COPON DE NEGRO PA TI";
+	// }
 ?>
