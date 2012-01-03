c86340dee58c35ce2b649b4b5c24beb5cc8a91f1
albertosola@ono.com <albertosola@ono.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-03 21:50:22 +0000

    JS para validar el fornulario de nuevas respuestas y controles para administrador o usuario

---
+++ trunk/ABD/includes/styles/js/validaFormularioRespuesta.js
@@ -0,0 +1,15 @@
+function controlAdministrador(valor){
+	if (valor==1){
+		document.getElementById("modificaRespuesta").disabled=false;
+	}else{
+		document.getElementById("modificaRespuesta").disabled=true;
+	}
+	
+}
+function modificaCuerpo(cuerpo){
+	window.alert(cuerpo);
+}
+
+function validar(){
+	
+}
