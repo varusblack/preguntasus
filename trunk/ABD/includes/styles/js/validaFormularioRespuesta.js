f433f0eca96b6abf68abc5fb3464516bd0587f17
albertosola@ono.com <albertosola@ono.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-04 13:03:11 +0000

    Realiza controles sobre las respuestas introducidas.

--- trunk/ABD/includes/styles/js/validaFormularioRespuesta.js
+++ trunk/ABD/includes/styles/js/validaFormularioRespuesta.js
@@ -1,15 +1,16 @@
-function controlAdministrador(valor){
+function validaRes(){
+	var respuesta=document.getElementById("mi-respuesta").value;
+	if (respuesta==""){
+		window.alert("El campo Respuesta no puede estar vacio");
+		return false;
+	}
+}
+
+/*function controlAdministrador(valor){
 	if (valor==1){
 		document.getElementById("modificaRespuesta").disabled=false;
 	}else{
 		document.getElementById("modificaRespuesta").disabled=true;
 	}
 	
-}
-function modificaCuerpo(cuerpo){
-	window.alert(cuerpo);
-}
-
-function validar(){
-	
-}
+}*/
