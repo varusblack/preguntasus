0b6b08f5b2d594fce7a17d46dcadfd20e4ad789f
albertosola@ono.com <albertosola@ono.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-05 13:18:40 +0000

    Modulo para eliminar una respuesta, con control de la sesion

--- trunk/ABD/procesado/eliminaRespuesta.php
+++ trunk/ABD/procesado/eliminaRespuesta.php
@@ -1,21 +1,28 @@
 <?php
-
-include_once ("../includes/styles/templates/cabecera.php");
-require_once ("../includes/widgets/login.php");
-require_once ("../includes/widgets/barranavegacion.php");
+require_once ("../entidades/Elemento.php");
 require_once ("../accesosBD/conexionesBD.php");
 require_once ("../accesosBD/accesosElemento.php");
-require_once ("../entidades/Elemento.php");
-
+ 
 session_start();
-$conexion=crearConexion();
-$elemento=new Elemento();
-$idElemento=$_REQUEST['cod'];
-echo $idElemento;
-$elemento=encontrarElementoPorId($idElemento,$conexion);
+$eliminaRespuesta =$_SESSION['eliminaRespuesta'];
 
 
-?>
-<?php
-	require_once ("../includes/styles/templates/pie.php");
+if (isset($eliminaRespuesta)) {// si está creada en la sesion
+  	realizaEliminacion($eliminaRespuesta);
+  
+} else
+	Header("Location:../preguntaRespuesta.php");
+
+function realizaEliminacion($eliminaRespuesta){
+	/*HACER ¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+	 * $conexion=crearConexion();
+	$elemento=new Elemento();
+	$elemento=$nuevaRespuesta['elemento'];
+	$elemento->cuerpo=$nuevaRespuesta['respuesta'];
+	$elemento->idrespuesta=$nuevaRespuesta['idelemento'];
+	insertarElemento($elemento,$conexion);
+	cerrarConexion($conexion);
+	 * */
+	 
+}
 ?>
