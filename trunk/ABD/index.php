c7a5f55ae5ce89357772088e9eeb54c419236fab
varusblack@gmail.com <varusblack@gmail.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-07 11:01:01 +0000

    Añadida barra de usuario

--- trunk/ABD/index.php
+++ trunk/ABD/index.php
@@ -16,23 +16,26 @@
 	// if ($ruta == '/ABD/') {
 		// require_once ("./includes/widgets/login.php");
 	// }
-	require_once ("./includes/widgets/login.php");
-	require_once ("./includes/widgets/barranavegacion.php");
+	
+
 	/*
 	 * Se inicializa la sesion y se crea la variable de sesión usuario y errores
 	 * Se comprueba posteriormente que si la variable está creada se inicialize a ""
 	 * para evitar cambios en el tipo de usuario, ej 1 para el administrador
 	 */
 	session_start();
-	$usuario=$_SESSION['usuario'];
-	$errores=@$_SESSION['errores'];
-	if(!isset($usuario)){
-		$usuario['tipoUsuario']="";
-		$_SESSION['usuario']=$usuario;
-	}
 	
+	if(isset($_SESSION["usuario"])){
+		$usuario=$_SESSION["usuario"];
+		require_once ("./includes/widgets/barraUsuario.php");
+	}else{
+		require_once ("./includes/widgets/login.php");
+	}
+
+	require_once ("./includes/widgets/barranavegacion.php");	
 ?> 
 <div id="contenedor_cuerpo">
+	ContenedorCuerpo
 	<div id="preguntas">
 		<?php 		
 			$conexion = crearConexion();
@@ -60,17 +63,19 @@
 				foreach ($resTags as $tg) {
 					if ($contador == 1) {
 						$tag1 = $tg;
+						$contador = $contador+1;
 					}
 					if ($contador == 2) {
 						$tag2 = $tg;
+						$contador = $contador+1;
 					}
 					if ($contador == 3) {
 						$tag3 = $tg;
+						$contador = $contador+1;
 					}
 					if ($contador > 3) {
 						break;
 					}
-                                        $contador++;
 				}						
 				require("./includes/widgets/preguntas.php");
 			}				
@@ -78,11 +83,13 @@
 		?>		
 	</div>
 	<div id="columna">
+		Columna
 		<div id="cuadro_busqueda">
-			<label for="buscar">Buscar:</label>
+			Cuadro de busqueda <label for="buscar">Buscar:</label>
 			<input type="text" id="buscar" name="buscar"/>
 		</div>
 		<div id="etiquetas_mas_frecuentes">
+			Nube de etiquetas
 		</div>
 	</div>
 </div>
