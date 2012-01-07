33789db72b057d4924a3a0781eccfca95540e3e3
albertosola@ono.com <albertosola@ono.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-07 14:35:03 +0000

    Control Eliminacion

--- ABD/procesado/eliminaRespuesta.php
+++ ABD/procesado/eliminaRespuesta.php
@@ -5,12 +5,16 @@ require_once ("../accesosBD/accesosElemento.php");
  
 session_start();
 $eliminaRespuesta =$_SESSION['eliminaRespuesta'];
+$idpregunta= $_SESSION['codigoPregunta']; 
 
 // Control que evite acceder directamente a esta página
 if (isset($eliminaRespuesta)) {// si está creada en la sesion
   	realizaEliminacion($eliminaRespuesta);  
 } 
-Header("Location:../preguntaRespuesta.php");
+echo $idpregunta;
+Header("Location:../preguntaRespuesta.php?idsolicitado=".$idpregunta);
+exit();
+
 
 function realizaEliminacion($eliminaRespuesta){
 	$elemento=new Elemento();
