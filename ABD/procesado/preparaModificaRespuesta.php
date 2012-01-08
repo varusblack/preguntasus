78d66ffb4bc3ec3bdda359679c11a80f84be0bab
albertosola@ono.com <albertosola@ono.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-08 19:46:23 +0000

    Preparacion del formulario que modifica respuestas

--- ABD/procesado/preparaModificaRespuesta.php
+++ ABD/procesado/preparaModificaRespuesta.php
@@ -1,12 +1,12 @@
 <?php
 require_once ("../includes/styles/templates/cabecera.php");
-require_once ("../includes/widgets/login.php");
+/*require_once ("../includes/widgets/login.php");
 require_once ("../includes/widgets/barranavegacion.php");
 require_once ("../accesosBD/conexionesBD.php");
 require_once ("../accesosBD/accesosElemento.php");
 require_once ("../accesosBD/accesosUsuario.php");
 require_once ("../entidades/Elemento.php");
-require_once ("../entidades/Usuario.php");
+require_once ("../entidades/Usuario.php");*/
 
 session_start();
 $idElemento=$_REQUEST['cod'];
@@ -20,20 +20,17 @@ $usuario=obtenerUsuarioPorId($idautor,$conexion);
 
 cerrarConexion($conexion);
 ?>
-<form id="modificaRespuesta" name="Moificarr" method="post" action="modificaRespuesta.php">
-	<div id="cuadroeliminacion">
+<form id="modificaRespuesta" name="Modificar" method="post" action="modificaRespuesta.php">
+	<div id="cuadroModificacion">
 		<h2 class="rotulos">Modifique Los Datos De La Respuesta</h2>
 		<div id="div_datoUsuario">
-			<label class="autor" id="label_modificarRespuesta">Email Del Autor :  </label><?php echo $usuario->email;?>		
+			<label  id="label_modificaRespuesta">Email Del Autor :  </label><?php echo $usuario->email;?>		
 		</div>
-		<div id="div_respuesta">
-			<textarea class="respuesta" id="label_modificaRespuesta" name="mi-respuesta" tabindex="101" rows="5" cols="80" ><?php echo $elemento->cuerpo;?></textarea>
+		<div id="div_modifica_respuesta">
+			<textarea id="text_modificaRespuesta" name="mi-respuesta" tabindex="101" rows="5" cols="80" ><?php echo $elemento->cuerpo;?></textarea>
 		</div>		
 		<div id="div_botonConfirmar">
-			<input type="submit" value="Modificar" />
-		</div>
-		<div id="div_botonCancelar">
-			<input type="button" value="Cancelar" name ="cancelar" onClick="location.href='../preguntaRespuesta.php?'"/>			
+			<input type="submit" value="Modificar" name="botonModificar"/>
 		</div>
 	</div>	
 </form>
