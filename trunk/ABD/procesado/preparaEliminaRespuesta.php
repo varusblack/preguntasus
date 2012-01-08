5e584c194153cf71fb04b4c4a7497faf2a92710f
albertosola@ono.com <albertosola@ono.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-08 19:47:42 +0000

    Sin Cambios

--- trunk/ABD/procesado/preparaEliminaRespuesta.php
+++ trunk/ABD/procesado/preparaEliminaRespuesta.php
@@ -11,7 +11,7 @@ require_once ("../entidades/Usuario.php");
 
 session_start();
 $idElemento=$_REQUEST['cod'];
-$eliminaRespuesta=$_SESSION['eliminaRespuesta'];
+//$eliminaRespuesta=$_SESSION['eliminaRespuesta'];
 
 $conexion=crearConexion();
 $elemento=new Elemento();
@@ -22,18 +22,18 @@ $elemento=encontrarElementoPorId($idElemento,$conexion);
 $idautor=$elemento->idautor;
 $usuario=obtenerUsuarioPorId($idautor,$conexion);
 
-if(!isset($eliminarespuesta)){
+/*if(!isset($eliminarespuesta)){
 	$eliminaRespuesta['elemento']=$elemento;
 	$_SESSION['eliminaRespuesta']=$eliminaRespuesta;
 	$_SESSION['codigoPregunta']= $_REQUEST['codigoPregunta'];
 	
-}
+}*/
 
 cerrarConexion($conexion);
 ?>
 <form id="eliminaRespuesta" name="Confirmar" method="post" action="eliminaRespuesta.php">
 	<div id="cuadroeliminacion">
-		<h2 class="rotulos">Confirme o Rechaze La Eliminacion De La Siguiente Respuesta</h2>
+		<h2 class="rotulos">Confirme La Eliminacion De La Respuesta Seleccionada</h2>
 		<div id="div_datoUsuario">
 			<label class="autor" id="label_eliminaRespuesta">Email Del Autor :  </label><?php echo $usuario->email;?>		
 		</div>
@@ -42,9 +42,6 @@ cerrarConexion($conexion);
 		</div>		
 		<div id="div_botonConfirmar">
 			<input type="submit" value="Confirmar" />
-		</div>
-		<div id="div_botonCancelar">
-			<input type="button" value="Cancelar" name ="cancelar" onClick="location.href='../preguntaRespuesta.php?idsolicitado='"<?$idElemento?>/>			
 		</div>
 	</div>	
 </form>
