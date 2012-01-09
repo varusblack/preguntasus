3c61afbd15cdf8aa8cda250f0b38457d55bf1ae2
albertosola@ono.com <albertosola@ono.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-09 23:09:58 +0000

    Control sobre el usuario no logado Mejoras de CSS Control sobre los notice

--- ABD/procesado/preparaModificaRespuesta.php
+++ ABD/procesado/preparaModificaRespuesta.php
@@ -1,24 +1,32 @@
 <?php
 require_once ("../includes/styles/templates/cabecera.php");
-/*require_once ("../includes/widgets/login.php");
-require_once ("../includes/widgets/barranavegacion.php");
-require_once ("../accesosBD/conexionesBD.php");
-require_once ("../accesosBD/accesosElemento.php");
-require_once ("../accesosBD/accesosUsuario.php");
-require_once ("../entidades/Elemento.php");
-require_once ("../entidades/Usuario.php");*/
 
-session_start();
-$idElemento=$_REQUEST['cod'];
-$conexion=crearConexion();
+//$formulario = $_SESSION['formularioRespuesta']; // se crea una variable formulario y errores dentro de la sesion
+//$errores = $_SESSION['errores'];
+
+if (!isset($formularioModificaRespuesta)) { 
+  	$formularioModificaRespuesta['respuesta'] = ""; 
+	$idElemento=$_REQUEST['cod'];
+	$idPregunta=$_REQUEST['codigoPregunta'];
+	$formularioModificaRespuesta['id']=$idElemento;
+	$formularioModificaRespuesta['idpregunta']=$idPregunta;
+  	$_SESSION['formularioRespuesta'] = $formularioModificaRespuesta;
+}
+if (isset($errores) && count($errores)>0) { 
+  echo "<div id=\"div_errores\" class=\"error\">";
+  foreach($errores as $error) {
+    echo $error . "<br/>"; 
+  }
+  echo "</div>";
+}
 $elemento=new Elemento();
 $usuario=new Usuario();
+$conexion=crearConexion();
 $elemento=encontrarElementoPorId($idElemento,$conexion);
 $idautor=$elemento->idautor;
 $usuario=obtenerUsuarioPorId($idautor,$conexion);
-
-
 cerrarConexion($conexion);
+
 ?>
 <form id="modificaRespuesta" name="Modificar" method="post" action="modificaRespuesta.php">
 	<div id="cuadroModificacion">
@@ -27,12 +35,12 @@ cerrarConexion($conexion);
 			<label  id="label_modificaRespuesta">Email Del Autor :  </label><?php echo $usuario->email;?>		
 		</div>
 		<div id="div_modifica_respuesta">
-			<textarea id="text_modificaRespuesta" name="mi-respuesta" tabindex="101" rows="5" cols="80" ><?php echo $elemento->cuerpo;?></textarea>
+			<textarea id="text_modificaRespuesta" name="respuestaModificada" tabindex="101" rows="5" cols="80" ><?php echo $elemento->cuerpo;?></textarea>
 		</div>		
-		<div id="div_botonConfirmar">
-			<input type="submit" value="Modificar" name="botonModificar"/>
+		<div id="div_botonModificar">
+			<input id="botonModificar" type="submit" value="Modificar" name="botonModificar"/>
 		</div>
-	</div>	
+	</div>
 </form>
 
 <?php
