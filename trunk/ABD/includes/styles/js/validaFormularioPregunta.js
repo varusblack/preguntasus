62bf8fa6cd3bf62904c02a93ad3f4b7a727aeef9
albertosola@ono.com <albertosola@ono.com@0a0a0bf0-4637-69a3-6ae4-187efc564cd2>
2012-01-10 16:27:47 +0000

    Actualziar java script

--- trunk/ABD/includes/styles/js/validaFormularioPregunta.js
+++ trunk/ABD/includes/styles/js/validaFormularioPregunta.js
@@ -5,20 +5,23 @@ function validaPregunta(){
 	var titulo=document.getElementById("tituloPregunta").value;
 	var cuerpo =document.getElementById("cuerpoPregunta").value;
 	var tag = document.getElementById("tag").value;
-	comprobar_camposDatos_vacios(titulo,cuerpo,tag);
-	return false;
+	return comprobar_camposDatos_vacios(titulo,cuerpo,tag);
+	
 }
 
 function comprobar_camposDatos_vacios(titulo,cuerpo,tag){
 	
 		if(titulo==""){
 			window.alert("Campo TITULO NO puede esta VACIO");
+			return false;
 		}
 		if(cuerpo==""){
 			window.alert("Campo CUERPO NO puede estar VACIO");
+			return false;
 		}
 		if(tag==""){
 			window.alert("Campo TAG NO puede estar VACIO");
+			return false;
 		}
-			
+		return true;
 }
